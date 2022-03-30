d6a06a4eefafc8f97889e5053b6e0d7bdca1ba9f
BugFreeCompilation <bugfreecompilation@gmail.com>
2022-03-30 16:57:50 +0200

    adjusting error names

--- error/error.go
+++ error/error.go
@@ -9,8 +9,8 @@ type ServiceError struct {
 }
 
 var (
-	ErrDatabaseOpen    = errors.New("error_open_db")
-	ErrDatabaseQuery   = errors.New("error_query_db")
-	ErrDatabasePrepare = errors.New("error_preparing_db")
+	ErrDatabaseOpen    = errors.New("open_db")
+	ErrDatabaseQuery   = errors.New("query_db")
+	ErrDatabasePrepare = errors.New("preparing_db")
 	ErrDatabaseEntry   = errors.New("entry_not_exist")
 )
